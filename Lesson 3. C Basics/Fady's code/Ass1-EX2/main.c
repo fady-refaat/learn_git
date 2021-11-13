752ba63465302ee7110bec5eafe9bee021656969
FadyGH <FadyGH.co>
2021-11-13 16:33:05 +0200

    lesson 3 assi

---
+++ Lesson 3. C Basics/Fady's code/Ass1-EX2/main.c
@@ -0,0 +1,20 @@
+/*
+/*
+ * main.c
+ *
+ *      Author: Fady
+ */
+
+#include"stdio.h"
+int main()
+{
+	int x;
+	printf("Enter a integer: ");
+	fflush(stdin); fflush (stdout);
+	scanf("%d", &x );
+	//fflush(stdin); fflush (stdout);
+	printf("You entered: %d", x );
+
+}
+
+
